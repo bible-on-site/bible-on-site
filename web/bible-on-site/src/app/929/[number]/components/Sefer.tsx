458b12b15a9713d7f071a8ba843dc87442182569
doradsoft <doradsoft@gmail.com>
2024-06-27 00:10:58 +0300

    feat(sefer): added empty page at end

--- web/bible-on-site/src/app/929/[number]/components/Sefer.tsx
+++ web/bible-on-site/src/app/929/[number]/components/Sefer.tsx
@@ -40,62 +40,65 @@ const hePageSemantics: PageSemantics = {
 const Sefer = (props: { perekObj: PerekObj }) => {
   const perekObj = props.perekObj;
   const sefer = getSeferByName(perekObj.sefer);
-  const hePages = sefer.perakim.map((perek, perekIdx) => (
-    <React.Fragment key={perekIdx + 1}>
-      <section className={styles.page}>
-        <article className={styles.perekText}>
-          {perek.pesukim.map((pasuk, pasukIdx) => {
-            const pasukKey = pasukIdx + 1;
-            const pasukNumElement = (
-              <a className={styles.pasukNum}>{toLetters(pasukIdx + 1)}</a>
-            );
-            const pasukElement = pasuk.segments.map((segment, segmentIdx) => {
-              const segmentKey = pasukIdx + 1 + "-" + (segmentIdx + 1);
-              // TODO: merge qris sequnce like in 929/406
-              return (
-                <React.Fragment key={segmentKey}>
-                  <a className={segment.type == "qri" ? styles.qri : ""}>
-                    {segment.type == "ktiv" ? (
-                      segment.value
-                    ) : segment.type == "qri" ? (
-                      <>
-                        (<label></label>
-                        {segment.value})
-                      </>
-                    ) : segment.type == "ptuha" ? (
-                      Ptuah()
-                    ) : (
-                      Stuma()
+  const emptyPage = <section className={styles.page}></section>;
+  const pages = sefer.perakim
+    .map((perek, perekIdx) => (
+      <React.Fragment key={perekIdx + 1}>
+        <section className={styles.page}>
+          <article className={styles.perekText}>
+            {perek.pesukim.map((pasuk, pasukIdx) => {
+              const pasukKey = pasukIdx + 1;
+              const pasukNumElement = (
+                <a className={styles.pasukNum}>{toLetters(pasukIdx + 1)}</a>
+              );
+              const pasukElement = pasuk.segments.map((segment, segmentIdx) => {
+                const segmentKey = pasukIdx + 1 + "-" + (segmentIdx + 1);
+                // TODO: merge qris sequnce like in 929/406
+                return (
+                  <React.Fragment key={segmentKey}>
+                    <a className={segment.type == "qri" ? styles.qri : ""}>
+                      {segment.type == "ktiv" ? (
+                        segment.value
+                      ) : segment.type == "qri" ? (
+                        <>
+                          (<label></label>
+                          {segment.value})
+                        </>
+                      ) : segment.type == "ptuha" ? (
+                        Ptuah()
+                      ) : (
+                        Stuma()
+                      )}
+                    </a>
+                    {segmentIdx === pasuk.segments.length - 1 ||
+                    (segment &&
+                      (segment.type === "ktiv" || segment.type === "qri") &&
+                      segment.value.at(segment.value.length - 1) ===
+                        "־") ? null : (
+                      <span> </span>
                     )}
-                  </a>
-                  {segmentIdx === pasuk.segments.length - 1 ||
-                  (segment &&
-                    (segment.type === "ktiv" || segment.type === "qri") &&
-                    segment.value.at(segment.value.length - 1) ===
-                      "־") ? null : (
-                    <span> </span>
-                  )}
+                  </React.Fragment>
+                );
+              });
+              return (
+                <React.Fragment key={pasukKey}>
+                  {pasukNumElement}
+                  <span> </span>
+                  {pasukElement}
+                  <span> </span>
                 </React.Fragment>
               );
-            });
-            return (
-              <React.Fragment key={pasukKey}>
-                {pasukNumElement}
-                <span> </span>
-                {pasukElement}
-                <span> </span>
-              </React.Fragment>
-            );
-          })}
-        </article>
-      </section>
-    </React.Fragment>
-  ));
+            })}
+          </article>
+        </section>
+      </React.Fragment>
+    ))
+    .concat(emptyPage);
 
   return (
     <FlipBook
       className="he-book"
-      pages={hePages}
+      pages={pages}
       pageSemantics={hePageSemantics}
       direction="rtl"
     />
