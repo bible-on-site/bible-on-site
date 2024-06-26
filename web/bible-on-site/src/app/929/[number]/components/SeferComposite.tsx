3a07082c303d514fd92e9b0fa2495b679e473f89
doradsoft <doradsoft@gmail.com>
2024-06-26 14:45:23 +0300

    fix: set Sefer display to none and not only opacity

--- web/bible-on-site/src/app/929/[number]/components/SeferComposite.tsx
+++ web/bible-on-site/src/app/929/[number]/components/SeferComposite.tsx
@@ -1,7 +1,7 @@
 // components/ClientWrapper.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ReadModeToggler from "./ReadModeToggler";
 import Sefer from "./Sefer";
@@ -11,21 +11,29 @@ const ClientWrapper = (props: { perekObj: PerekObj; toggled: boolean }) => {
   const toggled = props.toggled;
   const [everToggled, setEverToggled] = useState(false);
   const [currentlyToggled, setCurrentlyToggled] = useState(false);
+  const [display, setDisplay] = useState("none");
 
   const handleToggle = (toggled: boolean) => {
     if (!everToggled && toggled) {
       setEverToggled(true);
     }
-    setCurrentlyToggled(toggled);
+    if (toggled) {
+      setDisplay("initial");
+      setCurrentlyToggled(true);
+    } else {
+      setTimeout(() => setDisplay("none"), 300); // Match the transition duration
+      setCurrentlyToggled(false);
+    }
   };
+
   if (toggled) {
     handleToggle(true);
   }
-
   return (
     <>
       <ReadModeToggler toggled={toggled} onToggle={handleToggle} />
       <div
+        style={{ display }}
         className={`${
           currentlyToggled ? "opacity-1" : "opacity-0"
         } transition-opacity duration-300 absolute z-[7] top-[72px] w-full h-[calc(100vh-72px)] bg-white`}
