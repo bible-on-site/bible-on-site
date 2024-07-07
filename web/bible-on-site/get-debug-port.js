f9acdb4f9ada406aafe9a1586f5b179a641d2751
doradsoft <doradsoft@gmail.com>
2024-07-07 23:43:57 +0300

    revert: getDebugPort function

--- web/bible-on-site/get-debug-port.js
+++ web/bible-on-site/get-debug-port.js
@@ -1,5 +1,8 @@
+// This function is to generate a debug port
+// Currently it is hardcoded to 9229 but it can be changed to any number
+// Currently it is being used in order to start (playwright) tests in a known port in order to later connect to that port and pull coverage data
 export function getDebugPort() {
-  return 3000;
+  return 9229;
 }
 // TODO: think if name is representative enough
 export function getRouterDebugPort() {
