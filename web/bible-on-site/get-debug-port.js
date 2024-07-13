9e488d3a67155b5713d13a3038f7c2ae2d53c3df
doradsoft <doradsoft@gmail.com>
2024-07-13 23:51:33 +0300

    refactor: remove redundant import

--- web/bible-on-site/get-debug-port.js
+++ web/bible-on-site/get-debug-port.js
@@ -1,12 +1,10 @@
-const { execSync } = require("node:child_process");
-
 // This function is to generate a debug port
-// Currently it is hardcoded to 9229 but it can be changed to any number
+// Currently it is hardcoded to 9220 but it can be changed to any number
 // Currently it is being used in order to start (playwright) tests in a known port in order to later connect to that port and pull coverage data
 export function getDebugPort() {
-	return 9220;
+  return 9220;
 }
 // TODO: think if name is representative enough
 export function getRouterDebugPort() {
-	return getDebugPort() + 1;
+  return getDebugPort() + 1;
 }
