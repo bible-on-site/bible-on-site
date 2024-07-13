60c421065a1d62b66288c693dffcc93f67e8f820
doradsoft <doradsoft@gmail.com>
2024-07-14 00:10:46 +0300

    add hebcalDateToNumber tests

--- web/bible-on-site/tests/unit/util/hebdates-util.test.ts
+++ web/bible-on-site/tests/unit/util/hebdates-util.test.ts
@@ -1,49 +1,110 @@
-import { parseNumericalDateToHebcalDate } from "../../../src/util/hebdates-util";
+import {
+  parseNumericalDateToHebcalDate,
+  hebcalDateToNumber,
+} from "../../../src/util/hebdates-util";
+import { HDate, months } from "@hebcal/core";
+
+describe("hebcalDateToNumber", () => {
+  describe("when leap year", () => {
+    describe("when 1 Sh'vat 5784", () => {
+      it("returns month 05", () => {
+        const hDate = new HDate(1, months.SHVAT, 5784);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57840501);
+      });
+    });
+    describe("when 21 Sivan 5784", () => {
+      it("returns month 09", () => {
+        const hDate = new HDate(21, months.SIVAN, 5784);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57840921);
+      });
+    });
+    describe("when 15 Adar I 5784", () => {
+      it("returns month 13", () => {
+        const hDate = new HDate(15, months.ADAR_I, 5784);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57841315);
+      });
+    });
+    describe("when 20 Adar II 5784", () => {
+      it("returns month 14", () => {
+        const hDate = new HDate(20, months.ADAR_II, 5784);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57841420);
+      });
+    });
+  });
+  describe("when non-leap year", () => {
+    describe("when 1 Sh'vat 5783", () => {
+      it("returns month 05", () => {
+        const hDate = new HDate(1, months.SHVAT, 5783);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57830501);
+      });
+    });
+    describe("when 21 Sivan 5783", () => {
+      it("returns month 09", () => {
+        const hDate = new HDate(21, months.SIVAN, 5783);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57830921);
+      });
+    });
+    describe("when 15 Adar 5783", () => {
+      it("returns month 06", () => {
+        const hDate = new HDate(15, months.ADAR_I, 5783);
+        const actual = hebcalDateToNumber(hDate);
+        expect(actual).toBe(57830615);
+      });
+    });
+  });
+});
+
 describe("parseNumericalDateToHebcalDate", () => {
-	describe("when leap year", () => {
-		describe("when month is 05", () => {
-			it("returns Sh'vat", () => {
-				const actual = parseNumericalDateToHebcalDate(57840501);
-				expect(actual.toString()).toBe("1 Sh'vat 5784");
-			});
-		});
-		describe("when month is 09", () => {
-			it("returns Sivan", () => {
-				const actual = parseNumericalDateToHebcalDate(57840921);
-				expect(actual.toString()).toBe("21 Sivan 5784");
-			});
-		});
-		describe("when month is 13", () => {
-			it("returns Adar I", () => {
-				const actual = parseNumericalDateToHebcalDate(57841315);
-				expect(actual.toString()).toBe("15 Adar I 5784");
-			});
-		});
-		describe("when month is 14", () => {
-			it("returns Adar II", () => {
-				const actual = parseNumericalDateToHebcalDate(57841420);
-				expect(actual.toString()).toBe("20 Adar II 5784");
-			});
-		});
-	});
-	describe("when non leap year", () => {
-		describe("when month is 05", () => {
-			it("returns Sh'vat", () => {
-				const actual = parseNumericalDateToHebcalDate(57830501);
-				expect(actual.toString()).toBe("1 Sh'vat 5783");
-			});
-		});
-		describe("when month is 09", () => {
-			it("returns Sivan", () => {
-				const actual = parseNumericalDateToHebcalDate(57830921);
-				expect(actual.toString()).toBe("21 Sivan 5783");
-			});
-		});
-		describe("when month is 6", () => {
-			it("returns Adar", () => {
-				const actual = parseNumericalDateToHebcalDate(57831315);
-				expect(actual.toString()).toBe("15 Adar 5783");
-			});
-		});
-	});
+  describe("when leap year", () => {
+    describe("when month is 05", () => {
+      it("returns Sh'vat", () => {
+        const actual = parseNumericalDateToHebcalDate(57840501);
+        expect(actual.toString()).toBe("1 Sh'vat 5784");
+      });
+    });
+    describe("when month is 09", () => {
+      it("returns Sivan", () => {
+        const actual = parseNumericalDateToHebcalDate(57840921);
+        expect(actual.toString()).toBe("21 Sivan 5784");
+      });
+    });
+    describe("when month is 13", () => {
+      it("returns Adar I", () => {
+        const actual = parseNumericalDateToHebcalDate(57841315);
+        expect(actual.toString()).toBe("15 Adar I 5784");
+      });
+    });
+    describe("when month is 14", () => {
+      it("returns Adar II", () => {
+        const actual = parseNumericalDateToHebcalDate(57841420);
+        expect(actual.toString()).toBe("20 Adar II 5784");
+      });
+    });
+  });
+  describe("when non leap year", () => {
+    describe("when month is 05", () => {
+      it("returns Sh'vat", () => {
+        const actual = parseNumericalDateToHebcalDate(57830501);
+        expect(actual.toString()).toBe("1 Sh'vat 5783");
+      });
+    });
+    describe("when month is 09", () => {
+      it("returns Sivan", () => {
+        const actual = parseNumericalDateToHebcalDate(57830921);
+        expect(actual.toString()).toBe("21 Sivan 5783");
+      });
+    });
+    describe("when month is 6", () => {
+      it("returns Adar", () => {
+        const actual = parseNumericalDateToHebcalDate(57831315);
+        expect(actual.toString()).toBe("15 Adar 5783");
+      });
+    });
+  });
 });
