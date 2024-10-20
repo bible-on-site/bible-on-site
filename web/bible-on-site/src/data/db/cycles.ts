38f321bf1b3f5d981ef93725570751c74753af78
doradsoft <doradsoft@gmail.com>
2024-10-20 23:26:15 +0300

    lint: fix biome issues

--- web/bible-on-site/src/data/db/cycles.ts
+++ web/bible-on-site/src/data/db/cycles.ts
@@ -2,9 +2,10 @@ import type { Cycles } from "./cycles-types";
 import * as cyclesDB from "./cycles.json";
 
 const cycles: Cycles = Array.from(
-	process.env.IS_TEST_ENV
-		? (eval("require('./cycles.json')") as Cycles)
-		: /* istanbul ignore next: will never be reached in testing env */ (cyclesDB as Cycles),
+  process.env.IS_TEST_ENV
+    ? // biome-ignore lint/security/noGlobalEval: for testing environment only
+      (eval("require('./cycles.json')") as Cycles)
+    : /* istanbul ignore next: will never be reached in testing env */ (cyclesDB as Cycles)
 );
 
 export { cycles };
--- web/bible-on-site/tests/util/jest/coverage/index.js
+++ web/bible-on-site/tests/util/jest/coverage/index.js
@@ -2,36 +2,36 @@ const { CoverageReporter } = require("@jest/reporters");
 const { CoverageReport } = require("monocart-coverage-reports");
 import { filterOutCoverageRedundantFiles } from "../../coverage/filter-out-coverage-redundant-files";
 class MonocartCoverageReporter extends CoverageReporter {
-	constructor(globalConfig, reporterOptions, reporterContext) {
-		super(globalConfig, reporterContext);
-		this.disabled = !globalConfig.collectCoverage;
-		this.coverageProvider = globalConfig.coverageProvider;
-		this.reporterOptions = reporterOptions;
-	}
+  constructor(globalConfig, reporterOptions, reporterContext) {
+    super(globalConfig, reporterContext);
+    this.disabled = !globalConfig.collectCoverage;
+    this.coverageProvider = globalConfig.coverageProvider;
+    this.reporterOptions = reporterOptions;
+  }
 
-	onTestResult() {
-		if (this.disabled) {
-			return;
-		}
-		super.onTestResult.apply(this, arguments);
-	}
+  onTestResult(...args) {
+    if (this.disabled) {
+      return;
+    }
+    super.onTestResult.apply(this, args);
+  }
 
-	async onRunComplete(testContexts, results) {
-		if (this.disabled) {
-			return;
-		}
-		await this._addUntestedFiles(testContexts);
+  async onRunComplete(testContexts, results) {
+    if (this.disabled) {
+      return;
+    }
+    await this._addUntestedFiles(testContexts);
 
-		const coverage = await this._sourceMapStore.transformCoverage(
-			this._coverageMap,
-		);
-		filterOutCoverageRedundantFiles(coverage.data);
-		this.reporterOptions.sourceFinder = this._sourceMapStore.sourceFinder;
-		const coverageReport = new CoverageReport(this.reporterOptions);
-		coverageReport.cleanCache();
-		await coverageReport.add(coverage);
-		await coverageReport.generate();
-	}
+    const coverage = await this._sourceMapStore.transformCoverage(
+      this._coverageMap
+    );
+    filterOutCoverageRedundantFiles(coverage.data);
+    this.reporterOptions.sourceFinder = this._sourceMapStore.sourceFinder;
+    const coverageReport = new CoverageReport(this.reporterOptions);
+    coverageReport.cleanCache();
+    await coverageReport.add(coverage);
+    await coverageReport.generate();
+  }
 }
 
 module.exports = MonocartCoverageReporter;
