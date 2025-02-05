b36e6f41721be8b912ec21d63eebf541252b2934
Dorad Levinshtein <doradsoft@gmail.com>
2025-02-06 01:29:11 +0200

    test: fix unit

--- web/bible-on-site/jest.config.js
+++ web/bible-on-site/jest.config.js
@@ -5,61 +5,54 @@
 
 import nextJest from "next/jest.js";
 const createJestConfig = nextJest({
-	// path to Next.js app to load next.config.js and .env files into test environment
-	dir: "./",
+  // path to Next.js app to load next.config.js and .env files into test environment
+  dir: "./",
 });
 const reports = ["raw", "text"];
 if (!process.env.CI) {
-	reports.push("html");
+  reports.push("html");
 }
 
 /** @type {import('jest').Config} */
 const config = {
-	clearMocks: true,
+  clearMocks: true,
 
-	collectCoverage: true,
-	coverageReporters: ["none"],
-	collectCoverageFrom: ["./src/**/*.{ts,tsx,css,scss,js,json}"],
-	setupFiles: ["./jest.setup.js"],
-	preset: "ts-jest",
-	reporters: [
-		"default",
-		[
-			"./tests/util/jest/coverage",
-			{
-				// logging: 'debug',
-				name: "Jest Monocart Coverage Report",
-				all: "./src",
-
-				sourcePath: {
-					"src/": "",
-				},
-
-				outputDir: "./coverage/unit",
-
-				reports: reports,
-			},
-		],
-	],
-	// globalTeardown: "./tests/util/jest/globalTeardown.js",
-	testEnvironment: "jsdom",
-	testMatch: ["**/tests/(unit|integration)/**/*.test.ts"],
-	// A map from regular expressions to paths to transformers
-	transform: {
-		"^.+\\.ts$": "ts-jest",
-	},
-	extensionsToTreatAsEsm: [".ts", ".json"],
+  collectCoverage: true,
+  coverageReporters: ["none"],
+  collectCoverageFrom: ["./src/**/*.{ts,tsx,css,scss,js,json}"],
+  setupFiles: ["./jest.setup.js"],
+  preset: "ts-jest",
+  reporters: [
+    "default",
+    [
+      "./tests/util/jest/coverage",
+      {
+        name: "Jest Monocart Coverage Report",
+        all: "./src",
+        outputDir: "./coverage/unit",
+        reports: reports,
+      },
+    ],
+  ],
+  testEnvironment: "jsdom",
+  testMatch: ["**/tests/(unit|integration)/**/*.test.ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    // This will be overwritten in hackJestConfig.
+  ],
+  extensionsToTreatAsEsm: [".ts", ".json"],
 };
 
 // work around https://github.com/vercel/next.js/issues/35634
 async function hackJestConfig() {
-	// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-	const nextJestConfig = await createJestConfig(config)();
-	// /node_modules/ is the first pattern, so overwrite it with the correct version
-	nextJestConfig.transformIgnorePatterns[0] =
-		"/node_modules/(?!gematry).+\\.js$";
+  // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+  const nextJestConfig = await createJestConfig(config)();
+  // modules within @hebcal and gematry to not be transformed.
+  nextJestConfig.transformIgnorePatterns[0] = "/node_modules/(?!(@hebcal|gematry)/)";
 
-	return nextJestConfig;
+  return nextJestConfig;
 }
 
 export default hackJestConfig;
