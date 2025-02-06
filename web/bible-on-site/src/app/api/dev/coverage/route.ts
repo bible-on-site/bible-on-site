369d46996dae76a03106dd4250795df00ddfb568
Dorad Levinshtein <doradsoft@gmail.com>
2025-02-06 15:46:05 +0200

    chore: add husky for pre-commit hooks and update package files + fix real lint issue found using husky

--- web/bible-on-site/src/app/api/dev/coverage/route.ts
+++ web/bible-on-site/src/app/api/dev/coverage/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 // Much of this project is server side rendered, so this is a brdige to get coverage data from the server
 // The issue is explained here: https://github.com/kwonoj/swc-plugin-coverage-instrument/issues/246
 // The solution idea is quite old and it was prototyped here: https://github.com/bahmutov/next-and-cypress-example/blob/master/pages/api/__coverage__.js
-export async function GET(request: Request) {
+export function GET(request: Request) {
   // allow only requests from localhost
   const host = request.headers.get("host");
   if (!host || (!host.includes("localhost") && !host.includes("127.0.0.1"))) {
