e66f0d40adc636e7c45d871814fa3b1eb48dbe77
Dorad Levinshtein <doradsoft@gmail.com>
2025-02-09 03:37:11 +0200

    fix: try to ignore all jsons inside db dir; ignoree linting issues in test environment

--- web/bible-on-site/src/data/db/sefarim.ts
+++ web/bible-on-site/src/data/db/sefarim.ts
@@ -3,7 +3,8 @@ import type { Sefarim } from "./tanah-view-types";
 
 const sefarim: Sefarim = Array.from(
 	process.env.IS_TEST_ENV
-		? (eval(
+		? // biome-ignore lint/security/noGlobalEval: didn't manage to ignore for the entire directory. this code runs just in testing env
+			(eval(
 				"require('./sefaria-dump-5784-sivan-4.tanah_view.json')",
 			) as Sefarim)
 		: /* istanbul ignore next: will never be reached in testing env */ (tanah as Sefarim),
