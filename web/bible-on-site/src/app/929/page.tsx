85c1d234d5c233390b8e8839a2e537cebe7ab4a9
Dorad Levinshtein <doradsoft@gmail.com>
2025-02-09 02:23:17 +0200

    chore: drop eslint, use biome, fix biome linter c found issues, make use of biome when bumping version to allign with pre-commit formatter which uses biome

--- web/bible-on-site/src/app/929/page.tsx
+++ web/bible-on-site/src/app/929/page.tsx
@@ -2,27 +2,24 @@ import { RedirectType, redirect } from "next/navigation";
 import { getTodaysPerekId } from "../../data/perek-dto";
 
 export default async function TodaysPerek({
-	searchParams,
+  searchParams,
 }: {
-	searchParams: Promise<Record<string, string | string[] | undefined>>;
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
 }) {
-	const resolvedSearchParams = await searchParams;
-	const perekId = getTodaysPerekId();
-	const params = new URLSearchParams();
+  const resolvedSearchParams = await searchParams;
+  const perekId = getTodaysPerekId();
+  const params = new URLSearchParams();
 
-	Object.entries(resolvedSearchParams).forEach(([key, value]) => {
-		if (Array.isArray(value)) {
-			value.forEach((v) => {
-				params.append(key, v);
-			});
-		} else if (value !== undefined) {
-			params.append(key, value);
-		}
-	});
+  for (const [key, value] of Object.entries(resolvedSearchParams)) {
+    if (Array.isArray(value)) {
+      for (const v of value) {
+        params.append(key, v);
+      }
+    } else if (value !== undefined) {
+      params.append(key, value);
+    }
+  }
 
-	const paramsString = params.toString();
-	redirect(
-		`/929/${perekId}${paramsString.length > 0 ? `?${paramsString}` : ""}`,
-		RedirectType.replace,
-	);
+  const paramsString = params.toString();
+  redirect(`/929/${perekId}${paramsString.length > 0 ? `?${paramsString}` : ""}`, RedirectType.replace);
 }
