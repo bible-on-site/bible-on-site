d9ef60c990ee7e0570ffbb2b69989c6c708d433b
Dorad Levinshtein <doradsoft@gmail.com>
2025-12-06 21:06:47 +0200

    fix: update swcPlugins configuration for compatibility with Webpack and Turbopack

--- web/bible-on-site/next.config.mjs
+++ web/bible-on-site/next.config.mjs
@@ -7,8 +7,13 @@ const nextConfig = {
 	experimental: {
 		esmExternals: true,
 		externalDir: true,
-		swcPlugins: process.env.TURBOPACK
-			? []
+		swcPlugins: !process.env.WEBPACK
+			? // Currently there are a few bugs with swc-plugin-coverage-instrument when using turbopack
+				// 1. It decorates next/font with some overhead which breaks a next constraint that next/font should be top level something
+				//    This can be solved quite easily by adding "layout.tsx" to the unstableExclude array (and maybe break layout.tsx in order to lose as least coverage data as possible)
+				// 2. It resolves source paths as just their name breaking coverage tree structure
+				//    TODO: open an issue in swc-plugin-coverage-instrument repo with an MCVE
+				[]
 			: [
 					[
 						"swc-plugin-coverage-instrument",
